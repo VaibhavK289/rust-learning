e2c0b5b379cf1df0c4c7c2da3fa2e627b500ee18
Vaibhav Kumar Kandhway <vaibhavkrkandhway@gmail.com>
2026-04-04 22:36:11 +0530

    Basic calculator implemented with type checks

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -3,4 +3,12 @@ fn main() {
     let y: f32 = 3.0;
 
     println!("x: {:.1}, y: {:.1}", x, y);
+
+    let tup: (i32, u32, f64) = (-22, 40, 5.5);
+    
+    let (x,y,z) = tup;
+    println!("The value of x is: {}, y is {} and z is {}", x, y, z);
+    let minus_twenty_two = tup.0;
+    println!("Value: {}", minus_twenty_two);
+
 }
