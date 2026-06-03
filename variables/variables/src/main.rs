5ddc0da85e1686a9a8d9022880787d7b597750ba
Vaibhav Kumar Kandhway <vaibhavkrkandhway@gmail.com>
2026-06-03 16:32:04 +0530

    variables basics complete

--- variables/variables/src/main.rs
+++ variables/variables/src/main.rs
@@ -4,4 +4,12 @@ fn main() {
 
     let mangoes: i32 = 33;
     println!("{}", mangoes);
+
+    let _fruits: i32 = apples + mangoes;
+
+    println!("Number of apples is {}", apples - 10);
+    println!(
+        "Number of apples is {1} and mangoes is {0}",
+        apples, mangoes
+    );
 }
