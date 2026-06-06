31d6e6edb7c5e815da3a4e7c84591f54f272822c
Vaibhav Kumar Kandhway <vaibhavkrkandhway@gmail.com>
2026-06-06 23:56:48 +0530

    Compiler directive complete

--- variables/variables/src/main.rs
+++ variables/variables/src/main.rs
@@ -39,6 +39,9 @@
 
 const TAX_RATE: f64 = 7.25;
 type Meters = i32;
+
+#[allow(unused_variables)]
+
 fn main() {
     let income: i32 = 10000;
     println!("My income is {income} and my tax rate is {TAX_RATE}");
@@ -49,4 +52,7 @@ fn main() {
     println!(
         "A one mile race is {mile_race_length} meters long and a two mile race is {two_mile_race_length} meters long"
     );
+
+    let total_calories: i32 = 3000;
+    let calories_consumed: i32 = 2750;
 }
