71a08db410dda42f551004519e25137af964ec04
Vaibhav Kumar Kandhway <vaibhavkrkandhway@gmail.com>
2026-06-14 23:41:35 +0530

    string and string literals complete

--- data_types/src/bin/datatypes.rs
+++ data_types/src/bin/datatypes.rs
@@ -4,10 +4,14 @@ fn main() {
     let var: i64 = 444443;
 
     let points_scored: i32 = 200;
+    let _days: usize = 55;
+    let _years: isize = -15_000;
 
     println!("{points_scored}");
 
     println!("{var}");
 
     println!("{var}");
+    println!("Dear Emily, \n How are you ?");
+    println!("Ankit said \"I went to stadium\"");
 }
