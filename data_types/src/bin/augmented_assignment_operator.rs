e29b9ced4b9fadd6a96be84fd85ab580456c4a29
Vaibhav Kumar Kandhway <vaibhavkrkandhway@gmail.com>
2026-06-18 01:50:39 +0530

    augmented assignment operator complete

---
+++ data_types/src/bin/augmented_assignment_operator.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut year: i32 = 2025;
+    year += 1;
+
+    println!("The new year is {year}");
+
+    let mut birth_year: i32 = 2003;
+    birth_year = birth_year + 1;
+
+    println!("{birth_year}");
+}
