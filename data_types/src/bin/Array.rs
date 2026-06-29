2b6039567781b5ce8b362bb6618b839adfbabbe5
Vaibhav Kumar Kandhway <vaibhavkrkandhway@gmail.com>
2026-06-29 23:37:55 +0530

    Arrays start and introduction complete

---
+++ data_types/src/bin/Array.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // Array is the fixed size datatype
+    let numbers: [i32; 6] = [4, 8, 15, 16, 23, 42];
+
+    let apples: [&str; 3] = ["Granny Smith", "McIntosh", "Red Delicious"];
+}
--- data_types/src/bin/equality_and_other_operators.rs
+++ data_types/src/bin/equality_and_other_operators.rs
@@ -8,4 +8,9 @@ fn main() {
     let plane_on_time: bool = false;
     let making_event: bool = purchase_ticket && plane_on_time;
     println!("It is {} that I will arrive as expected", making_event);
+
+    let user_has_paid_for_subscription: bool = true;
+    let user_is_admin: bool = true;
+    let user_can_see_premium_experience: bool = user_has_paid_for_subscription || user_is_admin;
+    println!("Can this user see my site? {user_can_see_premium_experience}");
 }
