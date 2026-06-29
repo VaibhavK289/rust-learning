2068d74502ebecb9dac1b691c69fcc5a6756d422
Vaibhav Kumar Kandhway <vaibhavkrkandhway@gmail.com>
2026-06-30 01:51:45 +0530

    Array indexes

--- data_types/src/bin/Array.rs
+++ data_types/src/bin/Array.rs
@@ -3,4 +3,10 @@ fn main() {
     let numbers: [i32; 6] = [4, 8, 15, 16, 23, 42];
 
     let apples: [&str; 3] = ["Granny Smith", "McIntosh", "Red Delicious"];
+    
+    let seasons: ["Spring", "Summer", "Fall", "Winter"];
+
+    let first: &str = seasons[0];
+    let second: &str = seasons[1];
+
 }
