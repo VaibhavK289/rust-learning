c76facdc4aa61febb15675a3d4b054a413fcbc2e
Vaibhav Kumar Kandhway <vaibhavkrkandhway@gmail.com>
2026-07-03 23:46:03 +0530

    traits complete

--- data_types/src/bin/traits.rs
+++ data_types/src/bin/traits.rs
@@ -4,4 +4,11 @@ fn main() {
     println!("{}", 5);
     println!("{}", 3.14);
     println!("{}", true);
+
+    println!("{:?}", seasons);
+
+    dbg!(seasons);
+
+    // do not leave any debug macros in the main production code because it is for benefit of
+    // developers and not intended to go in production.
 }
