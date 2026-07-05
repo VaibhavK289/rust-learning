ba6376531ace75b8a54e53c5c2f51b66b679d7cf
Vaibhav Kumar Kandhway <vaibhavkrkandhway@gmail.com>
2026-07-06 03:21:51 +0530

    Generics complete

--- data_types/src/bin/generics.rs
+++ data_types/src/bin/generics.rs
@@ -1,4 +1,10 @@
 fn main() {
-
     // A generic is a placeholder for an abstract type.
+    // Generic is a placeholder for a future generic type
+
+    let month_days: std::ops::Range<i32> = 1..31;
+
+    for month_day in month_days {
+        println!("{month_day}");
+    }
 }
