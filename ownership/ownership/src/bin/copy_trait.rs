1569e649cdcd1ff0ac5e09fc77b95fb91e1ec5cf
Vaibhav Kumar Kandhway <vaibhavkrkandhway@gmail.com>
2026-07-11 23:49:03 +0530

    COpy trait complete

--- ownership/ownership/src/bin/copy_trait.rs
+++ ownership/ownership/src/bin/copy_trait.rs
@@ -1,4 +1,12 @@
+//In this case since the value of both time and year is 2025 different copies of value 2025 would be
+//stored in the stack separatly.
+
+// The value is last to the stack would be removed first so in this case year value would be removed
+// first.
+
 fn main() {
     let time: i32 = 2025;
     let year: i32 = time;
+
+    println!("The time is {time}. It is the year {year}");
 }
