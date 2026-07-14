5b10121e3c1e21dcad15e4a9e13a4201d2ff180b
Vaibhav Kumar Kandhway <vaibhavkrkandhway@gmail.com>
2026-07-14 23:59:17 +0530

    Memory management continue

--- ownership/ownership/src/main.rs
+++ ownership/ownership/src/main.rs
@@ -1,4 +1,12 @@
 // Ownership is a set of compiler rules -- Ensures there are no memory bugs
+//
+// In c++ programmer is responsible for allocating and deallocating memory
+// Common errors in manual memory management is - 1. Forget to dellocate memory 2. Deallocating
+// already deallocated memory.
+//
+// Garbage collector is a program that runs parallel to our main program - it dellocates memory that
+// is not longer in use. Disadvantages - It can run at disadvantegous times and can slow down
+// execution of main program.
 
 fn main() {
     println!("Hello, world!");
