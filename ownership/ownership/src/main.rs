80b416594f881e491cf198891defba070c35db73
Vaibhav Kumar Kandhway <vaibhavkrkandhway@gmail.com>
2026-07-21 23:57:18 +0530

    Ownership concept

--- ownership/ownership/src/main.rs
+++ ownership/ownership/src/main.rs
@@ -7,6 +7,13 @@
 // Garbage collector is a program that runs parallel to our main program - it dellocates memory that
 // is not longer in use. Disadvantages - It can run at disadvantegous times and can slow down
 // execution of main program.
+//
+// Best of both worlds - speed and memory safety although it is a new paradigm and takes time to
+// master it.
+//
+//Program will not be compiled if the ownership principle is violated.
+//
+//Owner - Entity responsible for that resource cleaning up a piece of data.
 
 fn main() {
     println!("Hello, world!");
