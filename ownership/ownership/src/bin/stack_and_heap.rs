3e58e26235b7aee777d123e249854d4d66d2afcd
Vaibhav Kumar Kandhway <vaibhavkrkandhway@gmail.com>
2026-07-25 23:55:39 +0530

    stack and heap complete

--- ownership/ownership/src/bin/stack_and_heap.rs
+++ ownership/ownership/src/bin/stack_and_heap.rs
@@ -2,4 +2,10 @@
 // All stack data must be fixed and consistent size at the compile time.
 // Rust stores them in stack at runtime.
 //
+//
+//Reference size is constant.
+//
+//Reading data is faster in stack then heap.
+//Purpose of ownership is to assign resoponsibility for deallocating memory[primarily heap memeory].
+//
 fn main() {}
