7310c6ee7a8fb75a54e8c06316b26faff1ae056f
Vaibhav Kumar Kandhway <vaibhavkrkandhway@gmail.com>
2026-08-07 23:55:26 +0530

    Reference and burrowing notes

--- ownership/ownership/src/bin/references_and_burrowing.rs
+++ ownership/ownership/src/bin/references_and_burrowing.rs
@@ -1,3 +1,5 @@
+// Burrowing means using something without taking ownership of it.
+//Reference is an address in computer's memorywhere the original value is stored.
 fn main() {
     let my_stack_value: i32 = 2;
     let my_integer_reference: &i32 = &my_stack_value;
