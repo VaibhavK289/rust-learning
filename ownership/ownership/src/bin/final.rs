3f0bb2e2e2e9da34558ed00c308fbd81887aed80
Vaibhav Kumar Kandhway <vaibhavkrkandhway@gmail.com>
2026-08-19 23:52:23 +0530

    Ownership notes

--- ownership/ownership/src/bin/final.rs
+++ ownership/ownership/src/bin/final.rs
@@ -5,3 +5,8 @@ fn main() {}
 // String - A dynamic piece of text stored on the heap at runtime.
 //
 // &String ("ref String") - A reference to a heap String.
+//
+// Important - Ownership model is enforced by compiler. It applied to the variables storing values
+// in the heap. But if the data is stored in the heap and other functions using referencing then it
+// is stores its memeory address. This way it can use value of other variable without constraints of
+// ownership.
