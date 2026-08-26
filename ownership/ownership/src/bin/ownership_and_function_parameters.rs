beaae5ce0fc5da692005e31c8794fe7aae175f7f
Vaibhav Kumar Kandhway <vaibhavkrkandhway@gmail.com>
2026-08-26 23:33:24 +0530

    Function parameter notes

--- ownership/ownership/src/bin/ownership_and_function_parameters.rs
+++ ownership/ownership/src/bin/ownership_and_function_parameters.rs
@@ -4,4 +4,9 @@ fn main() {}
 // data it is and whether or not it implements the copy trait.
 //
 // For stack data like integers, floats and boolean which implements copy trait then Rust will
-// create a copy of the data
+// create a copy of the data because it follows the copy trait.
+//
+// For data stored in heap, like string the copy of the data is not created since, it does not
+// follow copy trait. Instead a reference to a stack is created.
+//
+//
