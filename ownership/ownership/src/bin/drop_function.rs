e0c1ed8813a73e4eb26a72a94425a0996af8de02
Vaibhav Kumar Kandhway <vaibhavkrkandhway@gmail.com>
2026-08-28 23:59:20 +0530

    drop function notes

--- ownership/ownership/src/bin/drop_function.rs
+++ ownership/ownership/src/bin/drop_function.rs
@@ -1,1 +1,9 @@
 fn main() {}
+
+//Concept Overview
+
+// In Rust, drop refers to two distinct but tightly linked mechanisms:
+
+// std::mem::drop (Function): An explicit call to end a value's lifetime immediately.
+
+// Drop (Trait): A mechanism to define custom cleanup logic (destructors) when a value's lifetime ends.
