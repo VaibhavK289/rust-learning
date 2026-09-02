dcc835f9047b66d06659794667382e82172772bb
Vaibhav Kumar Kandhway <vaibhavkrkandhway@gmail.com>
2026-09-03 00:00:01 +0530

    Notes

--- ownership/ownership/src/bin/stack_and_heap.rs
+++ ownership/ownership/src/bin/stack_and_heap.rs
@@ -8,4 +8,8 @@
 //Reading data is faster in stack then heap.
 //Purpose of ownership is to assign resoponsibility for deallocating memory[primarily heap memeory].
 //
+//
+//Heap memory is disorganised and have a memory allocator who allocates enough memory mostly more
+//than required to a data. Then if it expands during execution if it is inside the limits location
+//is not changed if not then location is changed.
 fn main() {}
